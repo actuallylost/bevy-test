110529215ec602f130d4c5f5927abbeacd295495
Lost <pandagaminggr@gmail.com>
2023-11-23 01:11:16 +0200

    feat: migrate to bevy tutorial structure; add flycam & editor

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,28 @@
 use bevy::prelude::*;
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin{
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-        .add_systems(Startup, add_people)
-        .add_systems(Update, greet_people);
-    }
-}
+use bevy_editor_pls::prelude::*;
+use bevy_flycam::{NoCameraPlayerPlugin, FlyCam};
 
 fn main() {
     App::new()
-    .add_plugins((DefaultPlugins, HelloPlugin))
+    .add_plugins((DefaultPlugins, EditorPlugin::default(), NoCameraPlayerPlugin))
+    .add_systems(Startup, (setup_cam, spawn_cubes))
     .run();
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+// System: kind of like a method in Unity's scripts
+fn setup_cam(mut commands: Commands) {
+    commands.spawn((Camera3dBundle::default(), FlyCam));
 }
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
+fn spawn_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
+    let mesh = mesh_assets.add(shape::Box::new(1., 1., 1.).into());
+    for x in -10..10 {
+        for z in -10..10 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                transform: Transform::from_translation(Vec3::new(x as f32 * 2., 0., z as f32 * 2.)),
+                ..Default::default()
+            });
         }
     }
 }
-
