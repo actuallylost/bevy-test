0ede1c374b3c4a168fef48320c85a91d50c59844
Lost <pandagaminggr@gmail.com>
2023-11-25 21:27:18 +0200

    chore: fix indentations

--- src/plugins/life_plugin.rs
+++ src/plugins/life_plugin.rs
@@ -3,23 +3,23 @@ use bevy::prelude::*;
 pub struct LifePlugin;
     
 impl Plugin for LifePlugin {
-        fn build(&self, _app: &mut App) {
-            todo!("Implement")
-        }
-
-        fn name(&self) -> &str {
-            todo!("Implement")
-        }
-
-        fn ready(&self, _app: &App) -> bool {
-            todo!("Implement")
-        }
-
-        fn finish(&self, _app: &mut App) {
-            todo!("Implement")
-        }
-
-        fn cleanup(&self, _app: &mut App) {
-            todo!("Implement")
-        }
+    fn build(&self, _app: &mut App) {
+        todo!("Implement")
     }
+
+    fn name(&self) -> &str {
+        todo!("Implement")
+    }
+
+    fn ready(&self, _app: &App) -> bool {
+        todo!("Implement")
+    }
+
+    fn finish(&self, _app: &mut App) {
+        todo!("Implement")
+    }
+
+    fn cleanup(&self, _app: &mut App) {
+        todo!("Implement")
+   }
+}
