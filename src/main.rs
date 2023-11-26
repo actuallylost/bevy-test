60062918451e890b09c19caf66066d52e629b025
Lost <pandagaminggr@gmail.com>
2023-11-26 18:21:35 +0200

    feat: rename Position to Tile; add TileBundle; refactor

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ impl Default for IsAlive {
         IsAlive(true)
     }
 }
+
 #[derive(Component)]
 struct IsNearLife(bool);
 
@@ -21,10 +22,20 @@ impl Default for IsNearLife {
         IsNearLife(false)
     }
 }
+
 #[derive(Component)]
-struct Position{
-    x: f32,
-    y: f32
+struct HasLife(bool);
+
+impl Default for HasLife {
+    fn default() -> Self {
+        HasLife(false)
+    }
+}
+
+#[derive(Component)]
+struct Tile {
+    x: u16,
+    y: u16
 }
 
 // Bundles
@@ -32,7 +43,7 @@ struct Position{
 struct LifeBundle {
     pub is_alive: IsAlive,
     pub is_near_life: IsNearLife,
-    pub position: Position
+    pub position: Tile
 }
 
 impl Default for LifeBundle {
@@ -40,46 +51,58 @@ impl Default for LifeBundle {
         LifeBundle {
             is_alive: IsAlive::default(),
             is_near_life: IsNearLife::default(),
-            position: Position{x: random::<f32>(), y: random::<f32>()}
+            position: Tile{x: random::<u16>(), y: random::<u16>()}
         }
     }
 }
 
+#[derive(Bundle)]
+struct TileBundle {
+    pub has_life: HasLife,
+    pub position: Tile
+}
+
+
 fn main() {
     App::new()
     .add_plugins((DefaultPlugins, EditorPlugin::default(), NoCameraPlayerPlugin))
-    .add_systems(Startup, (setup_cam, create_world, spawn_cubes))
+    .add_systems(Startup, create_world)
     .add_systems(Update, spawn_life)
     .run();
 }
 
-// TODO: Remove camera when game of life is implemented
-fn setup_cam(mut commands: Commands) {
-    commands.spawn((Camera3dBundle::default(), FlyCam));
-}
-
-// Create 2d plane
+// Create 2D plane
 fn create_world(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+    // Create grid
+    // commands.spawn(NodeBundle {
+    //     style: Style {
+    //         display: Display::Grid,
+    //         width: Val::Percent(100.0),
+    //         height: Val::Percent(100.0),
+    //     },
+    //     background_color: BackgroundColor(Color::WHITE),
+    //     ..default()
+    // });
 }
 
 
-// Spawns life into the world
+// Spawn life into the world
 fn spawn_life(mut commands: Commands) {
     let life = LifeBundle::default();
             
     commands.spawn(life);
 }
 
-fn spawn_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
-    let mesh = mesh_assets.add(shape::Box::new(1., 1., 1.).into());
-    for x in -10..10 {
-        for z in -10..10 {
-            commands.spawn(PbrBundle {
-                mesh: mesh.clone(),
-                transform: Transform::from_translation(Vec3::new(x as f32 * 2., 0., z as f32 * 2.)),
-                ..Default::default()
-            });
-        }
-    }
-}
+// fn spawn_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
+//     let mesh = mesh_assets.add(shape::Box::new(1., 1., 1.).into());
+//     for x in -10..10 {
+//         for z in -10..10 {
+//             commands.spawn(PbrBundle {
+//                 mesh: mesh.clone(),
+//                 transform: Transform::from_translation(Vec3::new(x as f32 * 2., 0., z as f32 * 2.)),
+//                 ..Default::default()
+//             });
+//         }
+//     }
+// }
