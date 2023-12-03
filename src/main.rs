493a04cb2fe0acc7287a864c878fc23587d8186f
Lost <pandagaminggr@gmail.com>
2023-12-03 18:05:49 +0200

    feat: add tile module for TilePlugin

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,11 @@
+pub mod tile;
+
 use bevy::prelude::*;
 use bevy_editor_pls::prelude::*;
-use bevy_flycam::{NoCameraPlayerPlugin, FlyCam};
-
-use rand::random;
-
-// Components
-#[derive(Component)]
-struct IsAlive(bool);
-
-impl Default for IsAlive {
-    fn default() -> Self {
-        IsAlive(true)
-    }
-}
-
-#[derive(Component)]
-struct IsNearLife(bool);
-
-impl Default for IsNearLife {
-    fn default() -> Self {
-        IsNearLife(false)
-    }
-}
-
-#[derive(Component)]
-struct HasLife(bool);
-
-impl Default for HasLife {
-    fn default() -> Self {
-        HasLife(false)
-    }
-}
-
-#[derive(Component)]
-struct Tile {
-    x: u16,
-    y: u16
-}
-
-// Bundles
-#[derive(Bundle)]
-struct LifeBundle {
-    pub is_alive: IsAlive,
-    pub is_near_life: IsNearLife,
-    pub position: Tile
-}
-
-impl Default for LifeBundle {
-    fn default() -> Self {
-        LifeBundle {
-            is_alive: IsAlive::default(),
-            is_near_life: IsNearLife::default(),
-            position: Tile{x: random::<u16>(), y: random::<u16>()}
-        }
-    }
-}
-
-#[derive(Bundle)]
-struct TileBundle {
-    pub has_life: HasLife,
-    pub position: Tile
-}
-
+use tile::*;
 
 fn main() {
     App::new()
-    .add_plugins((DefaultPlugins, EditorPlugin::default(), NoCameraPlayerPlugin))
-    .add_systems(Startup, create_world)
-    .add_systems(Update, spawn_life)
+    .add_plugins((DefaultPlugins, EditorPlugin::default(), TilePlugin))
     .run();
 }
-
-// Create 2D plane
-fn create_world(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-    // Create grid
-    // commands.spawn(NodeBundle {
-    //     style: Style {
-    //         display: Display::Grid,
-    //         width: Val::Percent(100.0),
-    //         height: Val::Percent(100.0),
-    //     },
-    //     background_color: BackgroundColor(Color::WHITE),
-    //     ..default()
-    // });
-}
-
-
-// Spawn life into the world
-fn spawn_life(mut commands: Commands) {
-    let life = LifeBundle::default();
-            
-    commands.spawn(life);
-}
-
-// fn spawn_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
-//     let mesh = mesh_assets.add(shape::Box::new(1., 1., 1.).into());
-//     for x in -10..10 {
-//         for z in -10..10 {
-//             commands.spawn(PbrBundle {
-//                 mesh: mesh.clone(),
-//                 transform: Transform::from_translation(Vec3::new(x as f32 * 2., 0., z as f32 * 2.)),
-//                 ..Default::default()
-//             });
-//         }
-//     }
-// }
---
+++ src/tile.rs
@@ -0,0 +1,84 @@
+use bevy::prelude::*;
+
+pub struct TilePlugin;
+
+impl Plugin for TilePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, create_world)
+        .add_systems(Update, spawn_life);
+    }
+}
+
+// Components
+#[derive(Component, Default, Clone, Copy)]
+pub struct IsAlive(bool);
+
+#[derive(Component, Default, Clone, Copy)]
+pub struct IsNearLife(bool);
+
+#[derive(Component, Default, Clone, Copy)]
+pub struct HasLife(bool);
+
+#[derive(Component, Default, Clone, Copy)]
+pub struct Tile {
+    pub x: u16,
+    pub y: u16
+}
+
+// Bundles
+#[derive(Bundle, Default, Clone, Copy)]
+pub struct TileBundle {
+    pub is_alive: IsAlive,
+    pub is_near_life: IsNearLife,
+    pub position: Tile
+}
+
+impl TileBundle {
+    fn new(is_alive: IsAlive, is_near_life: IsNearLife, position: Tile) -> Self {
+        Self {
+            is_alive,
+            is_near_life,
+            position
+        }
+    }
+
+    fn _get_is_alive(&self) -> IsAlive {
+        self.is_alive
+    }
+
+    fn _get_is_near_life(&self) -> IsNearLife {
+        self.is_near_life
+    }
+
+    fn _get_pos(&self) -> Tile {
+        self.position
+    }
+}
+
+// Create 2D plane
+fn create_world(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    // Create grid
+    todo!("Create grid");
+}
+
+
+// Spawn life into the world
+fn spawn_life(mut commands: Commands) {
+    let life = TileBundle::new(IsAlive(true), IsNearLife(false), Tile::default());
+    
+    commands.spawn(life);
+}
+
+// fn spawn_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
+//     let mesh = mesh_assets.add(shape::Box::new(1., 1., 1.).into());
+//     for x in -10..10 {
+//         for z in -10..10 {
+//             commands.spawn(PbrBundle {
+//                 mesh: mesh.clone(),
+//                 transform: Transform::from_translation(Vec3::new(x as f32 * 2., 0., z as f32 * 2.)),
+//                 ..Default::default()
+//             });
+//         }
+//     }
+// }
