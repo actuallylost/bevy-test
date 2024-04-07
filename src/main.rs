416ed9a24be1fe96f8f50401de6b52e2508da2ec
Lost <pandagaminggr@gmail.com>
2024-04-08 00:32:55 +0300

    feat: add CameraPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+mod camera;
 mod debug;
 mod movement;
 mod spaceship;
 
 use bevy::prelude::*;
+use camera::CameraPlugin;
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, SpaceshipPlugin, MovementPlugin, DebugPlugin))
+        .add_plugins((
+            DefaultPlugins,
+            SpaceshipPlugin,
+            CameraPlugin,
+            MovementPlugin,
+            DebugPlugin,
+        ))
         .run();
 }
