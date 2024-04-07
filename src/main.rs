c0d9b15e03a569e0191e6a3b3b42c695fa7fc85f
Lost <pandagaminggr@gmail.com>
2024-04-08 00:41:04 +0300

    feat: add delta time to position update (ep 2)

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,15 @@ use spaceship::SpaceshipPlugin;
 
 fn main() {
     App::new()
+        // Bevy built-ins
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
         .add_plugins((
             DefaultPlugins,
+            // Game's Plugins
             SpaceshipPlugin,
             CameraPlugin,
             MovementPlugin,
