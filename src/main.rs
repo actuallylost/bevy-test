d92bd37eec0a35554c853d1286fc1cc2bc1e2727
Lost <pandagaminggr@gmail.com>
2024-04-07 23:22:42 +0300

    feat: add spaceship spawning; updating position and printing position to console (ep1)

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,27 @@ struct Velocity {
 }
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_spaceship)
+        .add_systems(Update, (update_position, print_position))
+        .run();
+}
+
+fn spawn_spaceship(mut commands: Commands) {
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }));
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
+    for (velocity, mut position) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
+}
+
+fn print_position(query: Query<(Entity, &Position)>) {
+    // Log entity ID and Position of each entity with the "Position" component
+    for (entity, position) in query.iter() {
+        info!("Entity {:?} is at position {:?},", entity, position);
+    }
 }
