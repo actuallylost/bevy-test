8e06c8d4ed7155e2d67baaa6d6810090aeed8bde
Lost <pandagaminggr@gmail.com>
2024-04-08 00:48:00 +0300

    feat(ep3): add acceleration component; generalize SpaceshipBundle to MovingObjectBundle

--- src/movement.rs
+++ src/movement.rs
@@ -5,6 +5,30 @@ pub struct Velocity {
     pub value: Vec3,
 }
 
+impl Velocity {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Acceleration {
+    pub value: Vec3,
+}
+
+impl Acceleration {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Bundle)]
+pub struct MovingObjectBundle {
+    pub velocity: Velocity,
+    pub acceleration: Acceleration,
+    pub model: SceneBundle,
+}
+
 pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
